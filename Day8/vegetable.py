aa89f04d67e733212571b18b90f06dad706ce511
Vijayarani27 <41582647+Vijayarani27@users.noreply.github.com>
2021-02-26 22:00:58 +0530

    Update vegetable.py

--- Day8/vegetable.py
+++ Day8/vegetable.py
@@ -1,1 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Vegetable.sqlite3'
+
+db =SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class vegetable(db.Model):
+    vegname=db.Column(db.String(80), primary_key=True)
+    qty=db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<vegetable %r>' % self.vegname
+
+
+class vegSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = vegetable
+
+    vegname = ma.auto_field()
+    qty = ma.auto_field()
+
+
+db.create_all()
+schema=vegSchema()
+veg=vegetable(vegname='drumstick',qty=17)
+db.session.add(veg)
+db.session.commit()
+print(vegetable.query.all())
+print(schema.dump(veg))
